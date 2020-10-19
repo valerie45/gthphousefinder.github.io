e72cd2bd7b8318b882adda2a097e680038046064
Anton Gudiswitz <agudiswitz@gatech.edu>
2020-10-19 15:55:22 -0400

    Added route to load index.html

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express'); // declare dependencies
+const path = require('path');
 
 const app = express();   //create an instance of express
 
 const port = 3000;  //Express servers conventionally listen on port 3000
 
 app.get('/', (request, response) => {
-    response.send('Hi things are working :)');    
+    response.sendFile(path.join(__dirname, './index.html'));    
 });
 
 app.listen(port, () => {
