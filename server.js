912ad0ee95b5b641dc5e4b13c970c944f0f64234
Anton Gudiswitz <agudiswitz@gatech.edu>
2020-10-28 20:22:00 -0400

    Created basic javascript search function. Added testing data to search on.

--- server.js
+++ server.js
@@ -1,34 +1,37 @@
-const express = require('express'); // declare dependencies
+const express = require('express'); // import dependencies
 const path = require('path');
+const searchFile = require('./searchFile.js')
 
 const app = express();   //create an instance of express
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000  //Specify the port to lsiten on
 
-app.use(express.static(__dirname + '/public')); //allows .css files to be loaded in the Express server
-
-//**** Connects to PostgreSQL Database ****/
-const { Client } = require('pg');
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-client.connect();
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
+app.use(express.static(__dirname + '/public')); //allows files in /public to be loaded in the Express server
+app.use(express.static(__dirname + '/')); //allows all files to be loaded in the Express server (I think?)
 
 
+//******* ROUTES ************//
+
+//The landing page of the website.
 app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, './index.html'));    
 });
 
+//Runs when the user navigates to <some_url>/search. 
+//Currently runs a search, logs the results to console, and then displays the main page.
+app.get('/search', (request, response) => {
+
+	//Calls function search() in searchFile.js
+	//needs to be reconfigured to take in form data from index.html instead our written values here.
+	results = searchFile.search(0, 500000, ["30324"]);
+
+  
+	console.log("Search Results: ", results);
+	response.sendFile(path.join(__dirname, './index.html'));
+});
+
+
+
+//Server listens for URL addresses here.
 app.listen(PORT, () => {
     console.log(`Express server listening on port ${PORT}!`);
 });
