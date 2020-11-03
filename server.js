d46513f1c3f4ac9aefdf7dc485e81892ca1545c3
Anton Gudiswitz <agudiswitz@gatech.edu>
2020-11-02 19:55:25 -0500

    Added output for search results using ejs.

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const searchFile = require('./searchFile.js')
 const app = express();   //create an instance of express
 const PORT = process.env.PORT || 5000  //Specify the port to lsiten on
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'))
+
 app.use(express.static(__dirname + '/public')); //allows files in /public to be loaded in the Express server
 app.use(express.static(__dirname + '/')); //allows all files to be loaded in the Express server (I think?)
 
@@ -13,7 +16,8 @@ app.use(express.static(__dirname + '/')); //allows all files to be loaded in the
 
 //The landing page of the website.
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, './index.html'));    
+	// response.render("./index.html");
+	response.render('pages/index', { results });
 });
 
 //Runs when the user navigates to <some_url>/search. 
@@ -24,9 +28,9 @@ app.get('/search', (request, response) => {
 	//needs to be reconfigured to take in form data from index.html instead our written values here.
 	results = searchFile.search(0, 500000, ["30324"]);
 
-  
-	console.log("Search Results: ", results);
-	response.sendFile(path.join(__dirname, './index.html'));
+	//console.log("Search Results: ", results);
+	// response.sendFile(path.join(__dirname, './index.html'));
+	response.render('pages/index', { results });
 });
 
 
