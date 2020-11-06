bc58d94d6254d7812ba7ef6464143be521ffefc6
Anton Gudiswitz <agudiswitz@gatech.edu>
2020-11-06 00:35:38 -0500

    Linked html form to search fields. Added Impv as a search parameter.

--- searchFile.js
+++ searchFile.js
@@ -3,16 +3,29 @@ const fs = require('fs');
 //@param minimum total price.
 //@param maximum total price.
 //@param array of zipCodes to include. Default value is all 57 Fulton County GA zipcodes
-function search(totalPriceMin=0, totalPriceMax=9999999, zipCodes=["31106", "31193", "30213", "30268", "30272", "30291", "30296", "30301", "30303", "30305", "30304", "30307", "30306", "30309", "30308", "30311", "30310", "30313", "30312", "30315", "30314", "30316", "30319", "30318", "30321", "30324", "30326", "30325", "30328", "30004", "30327", "30005", "30332", "30331", "30334", "30009", "30337", "30336", "30339", "30342", "30344", "30022", "30349", "30350", "30354", "30358", "30363", "30375", "30384", "30380", "30388", "30398", "30396", "30076", "30075", "30097", "30098"]) {
-    console.log("Initiated search.");
+function search(totalPriceMin=0, totalPriceMax=10000000, zipCodes=["31106", "31193", "30213", "30268", "30272", "30291", "30296", "30301", "30303", "30305", "30304", "30307", "30306", "30309", "30308", "30311", "30310", "30313", "30312", "30315", "30314", "30316", "30319", "30318", "30321", "30324", "30326", "30325", "30328", "30004", "30327", "30005", "30332", "30331", "30334", "30009", "30337", "30336", "30339", "30342", "30344", "30022", "30349", "30350", "30354", "30358", "30363", "30375", "30384", "30380", "30388", "30398", "30396", "30076", "30075", "30097", "30098"],
+					imprvMax=10000000) {
+  
+	//If no input is passed in, fill in values
+	if (totalPriceMin == "" || totalPriceMin == null) {totalPriceMin = 0;}
+	if (totalPriceMax == "" || totalPriceMax == null) {totalPriceMax = 10000000;}
+	if (zipCodes == "" || zipCodes == null) {zipCodes = ["31106", "31193", "30213", "30268", "30272", "30291", "30296", "30301", "30303", "30305", "30304", "30307", "30306", "30309", "30308", "30311", "30310", "30313", "30312", "30315", "30314", "30316", "30319", "30318", "30321", "30324", "30326", "30325", "30328", "30004", "30327", "30005", "30332", "30331", "30334", "30009", "30337", "30336", "30339", "30342", "30344", "30022", "30349", "30350", "30354", "30358", "30363", "30375", "30384", "30380", "30388", "30398", "30396", "30076", "30075", "30097", "30098"];}
+	if (imprvMax == "" || imprvMax == null) {imprvMax = 10000000;}
+
+	var params = {
+		priceMin: totalPriceMin,
+		priceMax: totalPriceMin,
+		zip: zipCodes,
+		imprvMax: imprvMax
+	};
+
+	console.log(`Search request after cleaning: \nmin:${totalPriceMin} max:${totalPriceMax} zip:${zipCodes}`);
+
 
 	var output = [] //this will be our output
 
-    //Reads in the file
-    data = fs.readFileSync('./data/Tax_Parcel_TestData.json', 'utf-8');
-  
-	// converts the file into a local object
-	const histData = JSON.parse(data.toString());
+    data = fs.readFileSync('./data/Tax_Parcel_TestData.json', 'utf-8');    //Reads in the file
+	const histData = JSON.parse(data.toString());	// converts the file into a local object
 
 	//prints info about the file
 	console.log("Created JSON object:\n  Rows: ", histData.length, "\n  ZIP: ", zipCodes);
@@ -23,13 +36,15 @@ function search(totalPriceMin=0, totalPriceMax=9999999, zipCodes=["31106", "3119
 
 		//check conditions given in the parameters
 		if (zipCodes.includes(item.SITEZIP) &&									//check zipcode
-			item.TOT_APPR >= totalPriceMin && item.TOT_APPR <= totalPriceMax)	//check price
+			item.TOT_APPR >= totalPriceMin && 
+			item.TOT_APPR <= totalPriceMax &&
+			item.IMPR_APPR <= imprvMax)	//check price
 		{
 			output.push(item)	//if yes, add the object to the output
 			console.log("Address: ", item.SITEADDRESS, "   ZIP: ", item.SITEZIP, "   Total Price: ", item.TOT_APPR);
 		}
 	};
-	return(output); //return the data
+	return([output, params]); //return the data
 }
 
 //exports the function search to be used in server.js 
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express'); // import dependencies
+var bodyParser = require('body-parser');
 const path = require('path');
 const searchFile = require('./searchFile.js')
 
 const app = express();   //create an instance of express
 const PORT = process.env.PORT || 5000  //Specify the port to lsiten on
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'))
@@ -16,21 +19,23 @@ app.use(express.static(__dirname + '/')); //allows all files to be loaded in the
 
 //The landing page of the website.
 app.get('/', (request, response) => {
-	// response.render("./index.html");
+	results = null;
 	response.render('pages/index', { results });
 });
 
 //Runs when the user navigates to <some_url>/search. 
 //Currently runs a search, logs the results to console, and then displays the main page.
-app.get('/search', (request, response) => {
+app.post('/search', urlencodedParser, function(req, res) {
 
 	//Calls function search() in searchFile.js
-	//needs to be reconfigured to take in form data from index.html instead our written values here.
-	results = searchFile.search(0, 500000, ["30324"]);
 
-	//console.log("Search Results: ", results);
-	// response.sendFile(path.join(__dirname, './index.html'));
-	response.render('pages/index', { results });
+	// searchParameters = `zip:${req.body.zip} price min:${req.body.priceMin} price max:${req.body.priceMax} Improvement Max:${req.body.imprvMax}`;
+	out = searchFile.search(req.body.priceMin, req.body.priceMax, [String(req.body.zip)], req.body.imprvMax);
+	results = out[0]; 
+	params = out[1];
+	console.log(params.priceMin);
+
+	res.render('pages/index', { results, params });//Load the HTML page and pass the results
 });
 
 
